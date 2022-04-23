e0da7d45f8c81c9cb61428f217b30608cf163dff
Onur <onurbelek@outlook.com>
2022-04-23 21:18:44 +0100

    finished adding methods for modes/plants.js

--- api/models/plants.js
+++ api/models/plants.js
@@ -5,7 +5,7 @@ module.exports = class Plant {
         this.id = data.id;
         this.plantName = data.plantName
     };
-        
+
     static get all(){
         return new Promise (async (resolve, reject) => {
             try {
@@ -39,14 +39,40 @@ module.exports = class Plant {
     static async create(plant){
         return new Promise (async (resolve, reject) => {
             try {
-                let createdBook = await db.query(`INSERT INTO Plants (plant_name) VALUES ($1, $2, $3, $4) RETURNING *;`, [plant]);
-                let newBook = new Book(createdBook.rows[0]);
-                resolve  (newBook);
+                let createdPlant = await db.query(`INSERT INTO Plants (plant_name) VALUES ($1) RETURNING *;`, [plant]);
+                let newPlant = new Plant(createdPlant.rows[0]);
+                resolve  (newPlant);
             } catch (err) {
-                reject('Book could not be created');
+                reject('Plant could not be created');
                 console.log(err);
             }
         });
     };
-};
 
+    static update(data) {
+        return new Promise (async (resolve, reject) => {
+            try {
+                const {plantName, id} = data;
+                let updatedPlantData = await db.query(`UPDATE Plants 
+                                                       SET plant_name = $1 
+                                                       WHERE id = $2;`, [ plantName, id ]);
+                let updatedPlant = new Plant(updatedPlantData.rows[0]);
+                resolve (updatedPlant);
+            } catch (err) {
+                reject('Error updating Plant');
+            }
+        });
+    };
+
+    destroy(){
+        return new Promise(async(resolve, reject) => {
+            try {
+                const result = await db.query('DELETE FROM Plants WHERE id = $1 RETURNING *;', [ this.id ]);
+                resolve('Plant was deleted')
+            } catch (err) {
+                reject('Plant could not be deleted')
+                console.log(err)
+            }
+        })
+    };
+};
