117b772f8722bfd1d3f80cf102aa22429e4cf92d
Onur <onurbelek@outlook.com>
2022-04-23 12:32:38 +0100

    altered the database design - added foreign key constraints

---
+++ api/dbConfig/dev_seeds.sql
@@ -0,0 +1,27 @@
+INSERT INTO Plant (plant_name)
+VALUES
+('American Marigold'),
+('Annual Vinca'),
+('Bacopa'),
+()
+
+INSERT INTO User (username, pass)
+VALUES
+(
+    'Onur',
+    'Bruh123'
+),
+(
+    'Khari',
+    'Vibes123'   
+),
+(
+    'Libby',
+    'Plant123'
+),
+(
+    'Robyn',
+    'ILoveConstruction123'
+);
+
+INSERT INTO Tracker ()
--- db/create_tracker.sql
+++ db/create_tracker.sql
@@ -5,6 +5,10 @@ CREATE TABLE Tracker (
     habit varchar(50) NOT NULL,
     count INT NOT NULL,
     habit_date DATE,
-    FOREIGN KEY(userId) REFERENCES User(id),
+    FOREIGN KEY(userId) REFERENCES User(id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
     FOREIGN KEY(plantId) REFERENCES Plant(id)
+    ON DELETE RESTRICT 
+    ON UPDATE CASCADE
 );
