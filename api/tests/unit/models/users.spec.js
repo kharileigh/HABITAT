c7c1f3f45cd1b031b8cb45d26716f3ce16c5865b
Robyn <rshortland@hotmail.co.uk>
2022-04-27 15:54:05 +0100

    Added create user test to unit testing

--- api/tests/unit/models/users.spec.js
+++ api/tests/unit/models/users.spec.js
@@ -1,4 +1,4 @@
-const User = require("../../../models/users");
+const User = require('../../../models/user')
 
 jest.mock("../../../models/users");
 
@@ -7,4 +7,18 @@ jest.mock("pg");
 
 const db = require("../../../dbConfig/init");
 
-describe("User", () => {});
+describe('User', () => {
+    beforeAll(() => jest.clearAllMocks())
+
+    afterAll(() => jest.resetAllMocks())
+
+describe("create", () => {
+  test("it resolves with user on successful db query", async () => {
+    let userData = { user_name: 'Test User', user_password: 'password', user_email: 'test@email.co.uk'}
+    jest.spyOn(db, 'query')
+        .mockResolvedValueOnce({rows: [ userData] });
+    const result = await User.create('new User');
+    expect(result).toBeInstanceOf(User)
+  }
+
+})
