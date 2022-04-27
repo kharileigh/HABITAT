22a2af3f96eaf619c98a111bc2bd29d4a70684a0
Robyn <rshortland@hotmail.co.uk>
2022-04-27 16:32:38 +0100

    purged habits files and renamed tracker folders to events

--- api/tests/unit/models/users.spec.js
+++ api/tests/unit/models/users.spec.js
@@ -1,4 +1,4 @@
-const User = require('../../../models/user')
+const User = require("../../../models/user");
 
 jest.mock("../../../models/users");
 
@@ -7,18 +7,21 @@ jest.mock("pg");
 
 const db = require("../../../dbConfig/init");
 
-describe('User', () => {
-    beforeAll(() => jest.clearAllMocks())
+describe("User", () => {
+  beforeAll(() => jest.clearAllMocks());
 
-    afterAll(() => jest.resetAllMocks())
+  afterAll(() => jest.resetAllMocks());
 
-describe("create", () => {
-  test("it resolves with user on successful db query", async () => {
-    let userData = { user_name: 'Test User', user_password: 'password', user_email: 'test@email.co.uk'}
-    jest.spyOn(db, 'query')
-        .mockResolvedValueOnce({rows: [ userData] });
-    const result = await User.create('new User');
-    expect(result).toBeInstanceOf(User)
-  }
-
-})
+  describe("create", () => {
+    test("it resolves with user on successful db query", async () => {
+      let userData = {
+        user_name: "Test User",
+        user_password: "password",
+        user_email: "test@email.co.uk",
+      };
+      jest.spyOn(db, "query").mockResolvedValueOnce({ rows: [userData] });
+      const result = await User.create("new User");
+      expect(result).toBeInstanceOf(User);
+    });
+  });
+});
