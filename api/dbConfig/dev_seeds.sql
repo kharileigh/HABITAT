f66c1f0fe8e93af58d4c9e3743baa0595b8a378c
Onur <onurbelek@outlook.com>
2022-04-27 12:18:53 +0100

    changed users and plants dev seeds

--- api/dbConfig/dev_seeds.sql
+++ api/dbConfig/dev_seeds.sql
@@ -1,20 +1,32 @@
-INSERT INTO plants (plant_name, nickname)
+INSERT INTO plants (plant_name, nickname, frequency, count, updatedOn)
 VALUES
 (
     'Snake Plant',
-    'Jeffrey'
+    'Jeffrey',
+    10,
+    5,
+    '2022-04-20 12:05:06'
 ),
 (
     'Pothos',
-    'Adonis'
+    'Adonis',
+    20,
+    3,
+    '2022-04-21 13:05:06'
 ),
 (
     'Aloe Vera',
-    'Bruh'
+    'Bruh',
+    4,
+    2,
+    '2022-04-27 10:05:06'
 ),
 (
     'Philodendron Green',
     'Oof'
+    5,
+    2,
+    '2022-04-25 11:05:06'
 );
 
 INSERT INTO users (user_name, user_password, user_email)
@@ -26,42 +38,18 @@ VALUES
 ),
 (
     'goodvibes',
-    '$2y$12$Nu9OPRgbeYDS1BIuLFIYmuYMSyUMNj6nyBUHuDBEll5VGPUMW8Iwe',
+    '$2y$12$Nu9OPRgbeYDS1BIuLOEYmuYMSyUMNj6nyBUHuDBEll5VGPUMW8Iwe',
     'khari@outlook.com'  
 ),
 (
     'ILovePlants',
-    '$2y$12$Nu9OPRgbeYDS1BIuLFIYmuYMSyUMNj6nyBUHuDBEll5VGPUMW8Iwe',
+    '$2y$12$Nu9OPRgbeYDS1BIuLBIYmuYMSyUMNj6nyBUHuDBEll5VGPUMW8Iwe',
     'libby@outlook.com'
 ),
 (
     'sffsaj1',
-    '$2y$12$Nu9OPRgbeYDS1BIuLFIYmuYMSyUMNj6nyBUHuDBEll5VGPUMW8Iwe',
+    '$2y$12$Nu9OPRgbeYDS1BIuLJIYmuYMSyUMNj6nyBUHuDBEll5VGPUMW8Iwe',
     'robyn@outlook.com'
-);
-
-INSERT INTO habits (habit, count, frequency)
-VALUES
-(
-    'water',
-    4,
-    5,
-    ''
-),
-(
-    'repot',
-    2,
-    6
-),
-(
-    'take plant for a walk',
-    6,
-    10
-),
-(
-    'make plant greener',
-    15,
-    20
 );
 
 INSERT INTO events (plantId, habitId, userId)
