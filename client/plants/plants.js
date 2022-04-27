82001b6def69322091b0c4e7cad8d1dda163fc55
Robyn <rshortland@hotmail.co.uk>
2022-04-27 21:53:32 +0100

    Updated the Navbar

--- client/plants/plants.js
+++ client/plants/plants.js
@@ -1,42 +1,40 @@
 //  ---- GET REQUEST TO GET PLANTS FROM DATABASE ---- //
 
 function fetchData() {
-    fetch("http://localhost:3000/plants")
-        .then(response => {
+  fetch("http://localhost:3000/plants")
+    .then((response) => {
+      console.log(response);
+      if (!response.ok) {
+        throw Error("ERROR");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      // --- guard condition | check this for immediately fails first --- //
+      console.log(data);
+      if (data.length === 0) {
+        // window.location.href = "createPlant.html"
+      }
 
-            console.log(response);
-            if(!response.ok) {
-                throw Error("ERROR");
-            }
-            return response.json();
-        })
-        .then(data => {
-            // --- guard condition | check this for immediately fails first --- //
-            console.log(data);
-            if (data.length === 0) {
-                // window.location.href = "createPlant.html"
-            }
-
-            // ---- map gives back an array of Plant with its details as on object ---- //
-            console.log(data);
-            const html = data
-                .map(plant => {
-                    return `
+      // ---- map gives back an array of Plant with its details as on object ---- //
+      console.log(data);
+      const html = data
+        .map((plant) => {
+          return `
                         <p> ${plant.nickname} </p>
                     `;
-                })
-                .join("");
-
-            document
-                .querySelector("#plantInfo")
-                .insertAdjacentHTML('afterbegin', html);
-
         })
-        // ----- REDIRECT USER HERE TO CREATE PLANT PAGE???
-        .catch(error => {
-            console.log(error);
-        });
-}     
+        .join("");
+
+      document
+        .querySelector("#plantInfo")
+        .insertAdjacentHTML("afterbegin", html);
+    })
+    // ----- REDIRECT USER HERE TO CREATE PLANT PAGE???
+    .catch((error) => {
+      console.log(error);
+      window.location.href = "createPlant.html";
+    });
+}
 
 fetchData();
-
