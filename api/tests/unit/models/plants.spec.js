4e33b66db9e5500df1ffee3255c749ebd252b04c
LibertySprackling <101059469+LibertySprackling@users.noreply.github.com>
2022-04-29 12:36:26 +0100

    fixing resposnive design

--- api/tests/unit/models/plants.spec.js
+++ api/tests/unit/models/plants.spec.js
@@ -5,41 +5,39 @@ jest.mock("../../../models/plants");
 const pg = require("pg");
 jest.mock("pg");
 
-const db = require("../../../dbConfig/init");
+const db = require("../../../dbConfig/init")
 
 describe("Plant", () => {
   beforeEach(() => jest.clearAllMocks());
 
   afterAll(() => jest.resetAllMocks());
 
-  describe("all", () => {
-    test("it resolves with plants on successful db query", async () => {
-      jest.spyOn(db, "query").mockResolvedValueOnce({ rows: [{}, {}, {}] });
-      const all = await Plant.all;
-      expect(all).toHaveLength(3);
-    });
+  describe('all', () => {
+    test('it resolves with plants on successful db query', async () => {
+        jest.spyOn(db, 'query')
+            .mockResolvedValueOnce({ rows: [{}, {}, {}]});
+        const all = await Plant.all;
+        expect(all).toHaveLength(3)
+    })
+});
+
+  describe('findById', () => {
+    test('it resolves with plant on successful db query', async () => {
+        let plantData = { id: 1, plant_name: 'example 1', nickname: 'Test Plant 1', frequency: 12, count: 4}
+        jest.spyOn(db, 'query')
+            .mockResolvedValueOnce({rows: [ plantData ] });
+        const result = await Plant.findById(1);
+        expect(result).toBeInstanceOf(Plant)
+    })
   });
 
-  describe("findById", () => {
-    test("it resolves with plant on successful db query", async () => {
-      let plantData = { id: 1, title: "Test Plant 1" };
-      jest.spyOn(db, "query").mockResolvedValueOnce({ rows: [plantData] });
-      const result = await Plant.findById(1);
-      expect(result).toBeInstanceOf(Plant);
-    });
-  });
-
-  describe("create", () => {
-    test("it resolves with a new plant on successful db query", async () => {
-      let plantData = { plant_name: "Test Plant 4" };
-      jest
-        .spyOn(db, "query")
-        .mockResolvedValueOnce({ rows: [{ ...plantData, id: 1 }] });
-      jest
-        .spyOn(Author, "findOrCreateByName")
-        .mockResolvedValueOnce(new Plant({ id: 4, name: "Test Plant 4" }));
-      const result = await Plant.create(plantData);
-      expect(result).toHaveProperty("id");
-    });
-  });
+  describe('create', () => {
+    test('it resolves with plant on successful db query', async () => {
+      let plantData = { plantid: 2, plant_name: 'example 2', nickname: 'Test Plant 2', frequency: 12, count: 4}
+        jest.spyOn(db, 'query')
+            .mockResolvedValueOnce({rows: [ { ...plantData, plantid: 2 }] });
+        const result = await Plant.create(plantData);
+        expect(result).toHaveProperty('plantid')
+    })
+});
 });
